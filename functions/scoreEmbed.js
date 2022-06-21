0c0ac3a6dfe6113c9b98d6037d4a5b0e9f8121c8
LevKoganov <levk@playsmart.co.il>
2022-06-22 00:13:27 +0300

    Optimizing code + Adding UI

--- functions/scoreEmbed.js
+++ functions/scoreEmbed.js
@@ -1,35 +1,39 @@
 const { MessageEmbed } = require("discord.js");
 
-function scoreEmbed (player1, player2, score1, score2) {
+function scoreEmbed(player1, player2, score1, score2) {
+  const emptySpace = "\u200B \u200B \u200B \u200B \u200B \u200B \u200B" // Empty space
+  const player1Info = `**__PLAYER1__ (${score1})${emptySpace} \n` +  "`1`" +`${player1}**`; // Player 1 string info
+  const player2Info = `**__PLAYER2__ (${score2})\n` + "`2`" + `${player2}**`; // Player 2 string info
+
   return new MessageEmbed()
     .setColor("BLURPLE")
-    .setTitle(" *Match (best of 10)*")
+    .setTitle(" *Match (first to 10)*")
     .setAuthor({
       name: "lf2.co.il",
-      iconURL: 'attachment://julian_author.png',
+      iconURL: "attachment://julian_author.png",
       url: "https://lf2.co.il/",
     })
     .setThumbnail("attachment://bandit_icon.png")
-    .addFields([
+    .addFields(
       {
         name: "\u200B",
-        value: `${player1.username}(***${score1}***)`,
+        value: player1Info,
         inline: true,
       },
 
       {
         name: "\u200B",
-        value: "*--VS--*",
+        value: `***--VS--***${emptySpace}`,
         inline: true,
       },
 
       {
         name: "\u200B",
-        value: `${player2.username}(***${score2}***)`,
+        value: player2Info,
         inline: true,
-      },
-    ])
+      }
+    )
     .setTimestamp();
-};
+}
 
-module.exports = scoreEmbed
+module.exports = scoreEmbed;
--- functions/teamScoreEmbed.js
+++ functions/teamScoreEmbed.js
@@ -1,11 +1,13 @@
 const { MessageEmbed } = require("discord.js");
 
 function scoreEmbed(player1, player2, player3, player4, t1_score, t2_score) {
-  const emptySpace = "\u200B \u200B \u200B \u200B \u200B \u200B \u200B \u200B \u200B \u200B \u200B \u200B \u200B \u200B \u200B \u200B"
+  const emptySpace = "\u200B \u200B \u200B \u200B \u200B \u200B \u200B" // Empty space
+  const team1Info = `**__TEAM1__ (${t1_score})**${emptySpace} \n` + "`1`" + `${player1}\n` + "`2`" + `${player2}` // Player 1 string info
+  const team2Info = `**__TEAM2__ (${t2_score})**\n` + "`3`" + `${player3}\n` + "`4`" + `${player4}` // Player 2 string info
 
   return new MessageEmbed()
     .setColor("BLURPLE")
-    .setTitle(" *Group match (best of 10)*")
+    .setTitle(" *Group match (first to 10)*")
     .setAuthor({
       name: "lf2.co.il",
       iconURL: "attachment://julian_author.png",
@@ -16,18 +18,18 @@ function scoreEmbed(player1, player2, player3, player4, t1_score, t2_score) {
     .addFields(
       {
         name: "\u200B",
-        value: `__TEAM1(***${t1_score}***)__${emptySpace} \n${player1}\n${player2}`,
+        value: team1Info,
         inline: true,        
       },
 
       {
         name: "\u200B",
-        value: `*| --VS-- |*${emptySpace}`,
+        value: `***--VS--***${emptySpace}`,
         inline: true,
       },
       {
         name: "\u200B",
-        value: `__TEAM2(***${t2_score}***)__\n${player3}\n${player4}`,
+        value: team2Info,
         inline: true,
       }
     )
