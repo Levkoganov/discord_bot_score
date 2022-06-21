3981d7d04cbbf2e8608bb49b593b787208c5af9a
LevKoganov <levk@playsmart.co.il>
2022-06-22 00:10:34 +0300

    Adding UI

--- commands/start.js
+++ commands/start.js
@@ -31,6 +31,9 @@ module.exports = {
       const authorImg = new MessageAttachment("./public/img/julian_author.png");
       const iconImg = new MessageAttachment("./public/img/bandit_icon.png");
 
+      // Empty space variable
+      const emptySpace = "\u200B \u200B \u200B \u200B \u200B \u200B \u200B";
+
       // All players
       const p1 = interaction.options.getUser("player1");
       const p2 = interaction.options.getUser("player2");
@@ -99,13 +102,16 @@ module.exports = {
         // Player1 btn
         if (i.customId === p1.username) {
           p1_score++; // Increment counter when btn clicked
+          let btn1_player1Update = `**__PLAYER1__ (${p1_score})${emptySpace} \n` + "`1`" + `${p1}**`;
+          let btn1_player2Update = `*~~__PLAYER2__ (${p2_score})\n` + "`2`" + `${p2}~~*`;
 
           // Player1 win
           if (p1_score === 10) {
-            cardEmbed.fields[0].value = `*${p1.username}(**${p1_score}**)*`; // Winner field
-            cardEmbed.fields[2].value = `*~~${p2.username}(**${p2_score}**)~~*`; // Loser field
-            cardEmbed.setTitle(`${Winneremoji}${p1.username}`);
+            cardEmbed.fields[0].value = btn1_player1Update; // Winner field
+            cardEmbed.fields[2].value = btn1_player2Update; // Loser field
+            cardEmbed.setTitle(`${Winneremoji}*${p1.username}*`);
             cardEmbed.setThumbnail(p1_avatar);
+
             return await i.update({
               embeds: [cardEmbed],
               files: [authorImg],
@@ -114,20 +120,24 @@ module.exports = {
           }
 
           // Update player1 embed
-          cardEmbed.fields[0].value = `${p1.username}(***${p1_score}***)`; // Edit embed field
+          cardEmbed.fields[0].value = btn1_player1Update; // Edit embed field
           await i.update({ embeds: [cardEmbed] });
         }
 
         // Player2 btn
         if (i.customId === p2.username) {
+          
           p2_score++; // Increment counter when btn clicked
+          let btn2_player1Update = `*~~__PLAYER1__ (${p1_score})~~${emptySpace} \n` + "~~`1`" + `${p1}~~*`
+          let btn2_player2Update = `**__PLAYER2__ (${p2_score})\n` + "`2`" + `${p2}**`;
 
           // Player2 win
           if (p2_score === 10) {
-            cardEmbed.fields[2].value = `*${p2.username}(**${p2_score}**)*`; // Winner field
-            cardEmbed.fields[0].value = `*~~${p1.username}(**${p1_score}**)~~*`; // Loser field
-            cardEmbed.setTitle(`${Winneremoji}${p2.username}`);
+            cardEmbed.fields[2].value = btn2_player2Update; // Winner field
+            cardEmbed.fields[0].value = btn2_player1Update; // Loser field
+            cardEmbed.setTitle(`${Winneremoji}*${p2.username}*`);
             cardEmbed.setThumbnail(p2_avatar);
+            
             return await i.update({
               embeds: [cardEmbed],
               files: [authorImg],
@@ -136,7 +146,7 @@ module.exports = {
           }
 
           // Update player1 embed
-          cardEmbed.fields[2].value = `${p2.username}(***${p2_score}***)`; // Edit embed field
+          cardEmbed.fields[2].value = btn2_player2Update; // Edit embed field
           await i.update({ embeds: [cardEmbed] });
         }
       });
@@ -145,6 +155,7 @@ module.exports = {
       collector.on("end", (collected) =>
         console.log(`Collected ${collected.size} items`)
       );
+      
     } catch (err) {
       console.log(err);
 
