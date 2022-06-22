1162f44fe27f5d63f475e7a6026d7c05983ad5cb
LevKoganov <levk@playsmart.co.il>
2022-06-22 17:25:50 +0300

    Adding img parameter

--- functions/scoreEmbed.js
+++ functions/scoreEmbed.js
@@ -1,19 +1,19 @@
 const { MessageEmbed } = require("discord.js");
 
-function scoreEmbed(player1, player2, score1, score2) {
+function scoreEmbed(player1, player2, score1, score2, rounds, thumbnailImg) {
   const emptySpace = "\u200B \u200B \u200B \u200B \u200B \u200B \u200B" // Empty space
   const player1Info = `**__PLAYER1__ (${score1})${emptySpace} \n` +  "`1`" +`${player1}**`; // Player 1 string info
   const player2Info = `**__PLAYER2__ (${score2})\n` + "`2`" + `${player2}**`; // Player 2 string info
 
   return new MessageEmbed()
     .setColor("BLURPLE")
-    .setTitle(" *Match (first to 10)*")
+    .setTitle(`*Match (first to ${rounds})*`)
     .setAuthor({
       name: "lf2.co.il",
       iconURL: "attachment://julian_author.png",
       url: "https://lf2.co.il/",
     })
-    .setThumbnail("attachment://bandit_icon.png")
+    .setThumbnail(`attachment://${thumbnailImg}`)
     .addFields(
       {
         name: "\u200B",
--- functions/teamScoreEmbed.js
+++ functions/teamScoreEmbed.js
@@ -1,20 +1,20 @@
 const { MessageEmbed } = require("discord.js");
 
-function scoreEmbed(player1, player2, player3, player4, t1_score, t2_score) {
+function scoreEmbed(player1, player2, player3, player4, t1_score, t2_score, rounds, imgResult) {
   const emptySpace = "\u200B \u200B \u200B \u200B \u200B \u200B \u200B" // Empty space
   const team1Info = `**__TEAM1__ (${t1_score})**${emptySpace} \n` + "`1`" + `${player1}\n` + "`2`" + `${player2}` // Player 1 string info
   const team2Info = `**__TEAM2__ (${t2_score})**\n` + "`3`" + `${player3}\n` + "`4`" + `${player4}` // Player 2 string info
 
   return new MessageEmbed()
     .setColor("BLURPLE")
-    .setTitle(" *Group match (first to 10)*")
+    .setTitle(`*Group match (first to ${rounds})*`)
     .setAuthor({
       name: "lf2.co.il",
       iconURL: "attachment://julian_author.png",
       url: "https://lf2.co.il/",
     })
     
-    .setThumbnail("attachment://bandit_icon.png")
+    .setThumbnail(`attachment://${imgResult}`)
     .addFields(
       {
         name: "\u200B",
