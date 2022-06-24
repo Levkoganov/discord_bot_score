f1e7e6063c7baf0fa5f6905367660ec7c03ff301
LevKoganov <levk@playsmart.co.il>
2022-06-24 16:45:10 +0300

    function channel-data

---
+++ functions/getChannelData.js
@@ -0,0 +1,19 @@
+const setGuildChannel = require("../models/setGuildChannel-schema");
+
+//
+async function getChannelData(guildId, interaction) {
+  try {
+    // Check if channel exist in server
+    const result = await setGuildChannel.findById(guildId);
+    if(!result) return;
+    const { channelId } = result;
+
+    // Return channel information
+    return interaction.guild.channels.cache.get(channelId);
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = getChannelData;
