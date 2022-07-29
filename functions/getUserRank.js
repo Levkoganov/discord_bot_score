441c896f04f2388bccea2d021326299cda8b8d80
LevKoganov <levk@playsmart.co.il>
2022-07-29 16:25:47 +0300

    get user rank command

---
+++ functions/getUserRank.js
@@ -0,0 +1,58 @@
+// const { MessageAttachment } = require("discord.js");
+const setPlayerScoreSchema = require("../models/setPlayerScore-schema");
+
+// Update rank leaderboard
+async function getUserRank(user) {
+  try {
+    // Images
+    // const iconImg = new MessageAttachment("./public/img/julian_author.png");
+    // const authorImg = new MessageAttachment("./public/img/lf2_title_icon.png");
+    
+    const userInfo = await getUserInfo(user.id); // Get ranked players
+    return userInfo
+    
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// Query for top 10 players
+async function getUserInfo(userId) {
+  try {
+    return await setPlayerScoreSchema.aggregate([
+      {
+        $sort: { score: -1, win: -1, lose: 1 },
+      },
+      {
+        $group: { _id: "", items: { $push: "$$ROOT" } },
+      },
+      {
+        $unwind: { path: "$items", includeArrayIndex: "items.rank" },
+      },
+      { $replaceRoot: { newRoot: "$items" } },
+      {
+        $addFields: { rank: { $add: ["$rank", 1] } },
+      },
+      {
+        $sort: { score: -1, win: -1, lose: 1 },
+      },
+      {
+        $project: {
+          _id: "$_id",
+          name: "$name",
+          score: "$score",
+          rank: "$rank",
+          win: "$win",
+          lose: "$lose",
+        },
+      },
+      {
+        $match: { _id: userId }
+      },
+    ]);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = getUserRank;
