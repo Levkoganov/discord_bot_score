04ba399a826eea3f806b97fdb382db12d116d0c1
LevKoganov <levk@playsmart.co.il>
2022-07-29 16:29:55 +0300

    new rank player embed

---
+++ functions/embeds/playerRankEmbed.js
@@ -0,0 +1,29 @@
+const { MessageEmbed } = require("discord.js");
+
+function playerRankEmbed(rankedPlayersData) {
+  // inside a command, event listener, etc.
+  const firstPlace = "<:first_place:1001936928133873745>"; // Winner emoji
+  const secondPlace = "<:second_place:1001936945208889465>"; // Winner emoji
+  const thirdPlace = "<:third_place:1001936962862719026>"; // Winner emoji
+
+  const rankedEmbedData = new MessageEmbed()
+    .setColor("BLURPLE")
+    .setTitle("\u200B")
+    .setAuthor({
+      name: "Player information",
+      iconURL: "attachment://julian_author.png",
+    })
+
+    .setFooter({ text: 'based on leaderboard.' })
+
+  rankedPlayersData.forEach((data) => {
+    rankedEmbedData.addFields({
+      name: `${data.rank === 1 ? firstPlace : data.rank === 2 ? secondPlace : data.rank === 3 ? thirdPlace : 'Rank'.concat('`', data.rank, '`: ')} ${''.concat('__',data.name,'__')}`,
+      value: `Points:${data.score} | Win:${data.win} | Lose:${data.lose}`,
+    });
+  });
+
+  return rankedEmbedData;
+}
+
+module.exports = playerRankEmbed;
